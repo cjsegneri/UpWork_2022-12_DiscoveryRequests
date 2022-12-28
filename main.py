1547a2243f6fa230196a2f2fee01333e4f106f28
Connor Segneri <connorsegneri@gmail.com>
2022-12-28 14:07:16 -0700

    extract the list of discovery requests for each document

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import os
+import re
 from PyPDF2 import PdfFileReader
 
 def get_pdf_text(path):
@@ -13,13 +14,13 @@ def get_pdf_text(path):
         if i == 0:
             page = page.split('Filing #')[0]
 
-        pdf_list.append(page)
+        pdf_list.append(page+'  ')
     
     return ' '.join(pdf_list)
 
 def main():
-    #pdf_text = get_pdf_text('DiscoveryDocuments/BUCHANAN & BUCHANAN, P.A..pdf')
-    #pdf_text = get_pdf_text('DiscoveryDocuments/ANDREW J. GORMAN & ASSOCIATES.pdf')
+    #pdf_text = get_pdf_text('DiscoveryDocuments/Chartwell Law.pdf')
+    #print(pdf_text)
 
     # loop through given directory and collect all the file names
     path = 'DiscoveryDocuments/'
@@ -36,15 +37,27 @@ def main():
         discovery_text = get_pdf_text(pdf_path)
 
         discovery_text_list.append(discovery_text)
-
-    print(len(discovery_text_list))
     
     # manually verify the text for each document was read in correctly
-    #j = 0
-    #print('Document Name: ' + dir_list[j] + '\n\n\n')
-    #print(discovery_text_list[j])
+    # j = 56
+    # print('Document Name: ' + dir_list[j] + '\n\n\n')
+    # print(discovery_text_list[j])
 
     # for every document, extract the list of discovery requests
+    discovery_requests_list = []
+    for i in range(0, len(discovery_text_list)):
+        regex = r'[0-9][0-9]?\.[ \t]{1,}(?:.|\n)*?\.(?:[ ]{2,}|\t|\n)'
+
+        regex_matches = re.findall(regex, discovery_text_list[i])
+        
+        discovery_requests_list.append(regex_matches)
+
+    # manually verify the requests list for each document
+    
+
+    # clean the requests
+
+    # identify and remove stopwords
 
     return 0
 

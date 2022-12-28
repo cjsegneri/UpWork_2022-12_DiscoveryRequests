bee603c155065af2167b313477a90adde49896d5
Connor Segneri <connorsegneri@gmail.com>
2022-12-28 11:42:19 -0700

    add logic to loop through every pdf, and read in the raw text

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from PyPDF2 import PdfFileReader
 
 def get_pdf_text(path):
-    pdf_reader = PdfFileReader(path)
+    pdf_reader = PdfFileReader(path, strict=False)
 
     pdf_list = []
 
@@ -19,16 +19,30 @@ def get_pdf_text(path):
 
 def main():
     #pdf_text = get_pdf_text('DiscoveryDocuments/BUCHANAN & BUCHANAN, P.A..pdf')
-    pdf_text = get_pdf_text('DiscoveryDocuments/ANDREW J. GORMAN & ASSOCIATES.pdf')
+    #pdf_text = get_pdf_text('DiscoveryDocuments/ANDREW J. GORMAN & ASSOCIATES.pdf')
 
     # loop through given directory and collect all the file names
     path = 'DiscoveryDocuments/'
-    dir_list = os.listdir(path)
-    print(dir_list)
 
-    # get the text for every file name
+    dir_list = os.listdir(path)
+
+    #print(len(dir_list)) # 57 documents in total
+
+    # get the raw text for discovery document
+    discovery_text_list = []
     for i in range(0, len(dir_list)):
-        print(dir_list[i])
+        pdf_path = path + dir_list[i]
+
+        discovery_text = get_pdf_text(pdf_path)
+
+        discovery_text_list.append(discovery_text)
+
+    print(len(discovery_text_list))
+    
+    # manually verify the text for each document was read in correctly
+    #j = 0
+    #print('Document Name: ' + dir_list[j] + '\n\n\n')
+    #print(discovery_text_list[j])
 
     # for every document, extract the list of discovery requests
 

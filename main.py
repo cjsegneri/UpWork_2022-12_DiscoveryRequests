0c7339576bce9daf30b3d61a1c95b53b3b9ac4db
Connor Segneri <connorsegneri@gmail.com>
2022-12-28 11:20:35 -0700

    remove the footer on the first page

--- main.py
+++ main.py
@@ -7,14 +7,18 @@ def get_pdf_text(path):
     pdf_list = []
 
     for i in range(0,pdf_reader.numPages):
+        page = pdf_reader.getPage(i).extractText()
         # if it's the first page, remove the footer
+        if i == 0:
+            page = page.split('Filing #')[0]
 
-        pdf_list.append(pdf_reader.getPage(i).extractText())
+        pdf_list.append(page)
     
     return ' '.join(pdf_list)
 
 def main():
-    pdf_text = get_pdf_text('DiscoveryDocuments/BUCHANAN & BUCHANAN, P.A..pdf')
+    #pdf_text = get_pdf_text('DiscoveryDocuments/BUCHANAN & BUCHANAN, P.A..pdf')
+    pdf_text = get_pdf_text('DiscoveryDocuments/ANDREW J. GORMAN & ASSOCIATES.pdf')
 
     print(pdf_text)
 

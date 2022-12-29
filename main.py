00bb8df0edfa091cbe4fa7bc3b6ac8b7b7e089e4
Connor Segneri <connorsegneri@gmail.com>
2022-12-29 12:48:07 -0700

    remove pdfs that cannot be read in

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import os
 import re
+from statistics import mean
 from PyPDF2 import PdfFileReader
 
 def get_pdf_text(path):
@@ -19,27 +20,33 @@ def get_pdf_text(path):
     return ' '.join(pdf_list)
 
 def main():
-    #pdf_text = get_pdf_text('DiscoveryDocuments/Chartwell Law.pdf')
+    #pdf_text = get_pdf_text('DiscoveryDocuments/Reynolds Parrino Shadwick P.A..pdf')
     #print(pdf_text)
 
     # loop through given directory and collect all the file names
     path = 'DiscoveryDocuments/'
-
     dir_list = os.listdir(path)
-
     #print(len(dir_list)) # 57 documents in total
 
     # get the raw text for discovery document
     discovery_text_list = []
     for i in range(0, len(dir_list)):
         pdf_path = path + dir_list[i]
-
         discovery_text = get_pdf_text(pdf_path)
-
         discovery_text_list.append(discovery_text)
     
+    # find the indexes for the pdfs that cannot be read in
+    broken_pdf_indexes = []
+    for i in range(0, len(discovery_text_list)):
+        if 'the' not in discovery_text_list[i]:
+            broken_pdf_indexes.append(i)
+    
+    # remove pdfs that cannot be read in
+    dir_list = [x for i,x in enumerate(dir_list) if i not in broken_pdf_indexes]
+    discovery_text_list = [x for i,x in enumerate(discovery_text_list) if i not in broken_pdf_indexes]
+
     # manually verify the text for each document was read in correctly
-    # j = 56
+    # j = 52
     # print('Document Name: ' + dir_list[j] + '\n\n\n')
     # print(discovery_text_list[j])
 
@@ -47,20 +54,31 @@ def main():
     discovery_requests_list = []
     for i in range(0, len(discovery_text_list)):
         regex = r'[0-9][0-9]?\.[ \t]{1,}(?:.|\n)*?\.(?:[ ]{2,}|\t|\n)'
-
         regex_matches = re.findall(regex, discovery_text_list[i])
-        
         discovery_requests_list.append(regex_matches)
 
     # manually verify the requests list for each document
-    j = 0
-    print('Document Name: ' + dir_list[j] + '\n\n\n')
-    for i in range(0, len(discovery_requests_list[j])):
-        print('[[' + discovery_requests_list[j][i] + ']]\n')
+    # j = 1
+    # print('Document Name: ' + dir_list[j] + '\n\n\n')
+    # for i in range(0, len(discovery_requests_list[j])):
+    #     print('[[' + discovery_requests_list[j][i] + ']]\n')
+
+    # get summary statistics
+    # sum_list = []
+    # for i in range(0, len(discovery_requests_list)):
+    #     sum_list.append(len(discovery_requests_list[i]))
+    
+    # print(len(sum_list))
+    # print(sum_list)
+
+    # print(mean(sum_list))
+    # print(sum(sum_list))
 
     # clean the requests text
         # remove whitespace at the beginning and end of each request
         # replace all whitespace (spaces, tabs, newlines) with a single space
+        # lowercase all characters
+        # 
 
     # identify and remove stopwords
 

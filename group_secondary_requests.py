b9a7fa1a5cd1542eabce17a8baa88335370b1855
Connor Segneri <connorsegneri@gmail.com>
2023-01-06 16:12:46 -0700

    remove secondary stopwords

--- group_secondary_requests.py
+++ group_secondary_requests.py
@@ -20,15 +20,41 @@ def group_secondary_requests():
         # filter for the current group
         df_current_group = df_requests_with_groups.query('GroupID == '+str(i))
 
-        # NEW STOPWORD FILTER #
+        # get each request for the current group
+        current_requests = df_current_group['RequestCleanNoStop'].tolist()
+        # split each request into a unique list of words
+        current_requests_words = []
+        for j in range(0, len(current_requests)):
+            current_requests_words.append([*set(current_requests[j].split())])
+        # find the frequency for each word within the requests
+        word_freq = {}
+        for word_list in current_requests_words:
+            for word in word_list:
+                if word in word_freq:
+                    word_freq[word] += 1
+                else:
+                    word_freq[word] = 1
+        # filter out the most common words
+        unique_word_threshold = len(current_requests) *.9
+        secondary_grouping_stop_words = []
+        for key, value in word_freq.items():
+            if value > unique_word_threshold:
+                secondary_grouping_stop_words.append(key)
+        # remove the stopwords from each request
+        current_requests_no_stop = []
+        for j in range(0, len(current_requests_words)):
+            current_requests_no_stop.append(' '.join([wl for wl in current_requests_words[j] \
+                if wl not in secondary_grouping_stop_words]))
+        # add the requests with secondary stopwords removed as a new column in the df
+        df_current_group['RequestCleanNoStop_SecNoStop'] = current_requests_no_stop
 
         # run the similarity algorithm
         df_secondary_groups = group_similar_strings(
-            threshold = 95,
+            threshold = 90,
             min_group_size = 1,
             df = df_current_group,
             id_column_name = 'RequestID',
-            str_column_name = 'RequestCleanNoStop',
+            str_column_name = 'RequestCleanNoStop_SecNoStop',
             new_column_name = 'SecondaryGroupID')
 
         # join the groups data back to the orginal request df
@@ -38,15 +64,16 @@ def group_secondary_requests():
                 'SecondaryGroupID',
                 'DocumentName',
                 'RequestID',
+                'RequestCleanNoStop_SecNoStop',
                 'RequestClean',
                 'RequestCleanNoStop',
                 'RequestRaw']]
 
-        # if now rows have been appended yet to the empty dataframe, copy the current df
+        # if no rows have been appended to the empty dataframe, copy the current df
         if df_requests_with_secondary_groups.empty:
             df_requests_with_secondary_groups = df_current_group
+        # else, append the current df
         else:
-            # else, append the current df
             df_requests_with_secondary_groups = pd.concat([
                 df_requests_with_secondary_groups,
                 df_current_group])

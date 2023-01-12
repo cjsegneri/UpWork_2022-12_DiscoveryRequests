183ad8009b7ebfcb6c88fe77536227b6f338cba6
Connor Segneri <connorsegneri@gmail.com>
2023-01-12 16:53:24 -0700

    finish aggregating the rest of the final columns

--- aggregate_groups.py
+++ aggregate_groups.py
@@ -45,6 +45,18 @@ def aggregate_groups():
     sub_group_1_id = []
     sub_group_1_example_a = []
     sub_group_1_example_b = []
+    sub_group_2_id = []
+    sub_group_2_example_a = []
+    sub_group_2_example_b = []
+    sub_group_3_id = []
+    sub_group_3_example_a = []
+    sub_group_3_example_b = []
+    sub_group_4_id = []
+    sub_group_4_example_a = []
+    sub_group_4_example_b = []
+    sub_group_5_id = []
+    sub_group_5_example_a = []
+    sub_group_5_example_b = []
     for i in range(1, max_group_id+1):
         # filter for the current group id
         df_current_group = df_req_with_sec_groups.query('GroupID == '+str(i))
@@ -68,9 +80,9 @@ def aggregate_groups():
         word_counts_sorted = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)
         most_common_words = []
         for i in range(0,10):
-            most_common_words.append(word_counts_sorted[i][0])
+            if i < len(word_counts_sorted):
+                most_common_words.append(word_counts_sorted[i][0])
         primary_group_name.append(' '.join(most_common_words))
-        break
 
         # aggregate the subgroup1id
         min_sub_group_id = df_current_group['SecondaryGroupID'].min()
@@ -81,47 +93,116 @@ def aggregate_groups():
         df_current_sub_group = df_current_group.query('SecondaryGroupID == '+str(min_sub_group_id))
         sub_group_1_example_a.append(df_current_sub_group['RequestClean'].tolist()[0])
 
-        # aggregate the subgroup1examplb
+        # aggregate the subgroup1exampleb
         sub_group_1_example_b.append(df_current_sub_group['RequestClean'].tolist()[
             len(df_current_sub_group)-1])
 
-        # # aggregate the SubGroup2ID
-        # if max_sub_group_id - min_sub_group_id > 0:
-        #     sub_group_2_id.append(min_sub_group_id+1)
-        # else:
-        #     sub_group_2_id.append('NA')
+        # determine if there is a second subgroup
+        if max_sub_group_id - min_sub_group_id > 0:
+            # aggregate the subgroup2id
+            sub_group_2_id.append(min_sub_group_id+1)
+            # aggregate the subgroup2examplea
+            df_current_sub_group = df_current_group.query(
+                'SecondaryGroupID == '+str(min_sub_group_id+1))
+            sub_group_2_example_a.append(df_current_sub_group['RequestClean'].tolist()[0])
+            # aggregate the subgroup2exampleb
+            sub_group_2_example_b.append(df_current_sub_group['RequestClean'].tolist()[
+            len(df_current_sub_group)-1])
+        else:
+            sub_group_2_id.append('NA')
+            sub_group_2_example_a.append('NA')
+            sub_group_2_example_b.append('NA')
 
-        # # aggregate the SubGroup2Name
+        # determine if there is a third subgroup
+        if max_sub_group_id - min_sub_group_id > 1:
+            # aggregate the subgroup3id
+            sub_group_3_id.append(min_sub_group_id+2)
+            # aggregate the subgroup3examplea
+            df_current_sub_group = df_current_group.query(
+                'SecondaryGroupID == '+str(min_sub_group_id+2))
+            sub_group_3_example_a.append(df_current_sub_group['RequestClean'].tolist()[0])
+            # aggregate the subgroup2exampleb
+            sub_group_3_example_b.append(df_current_sub_group['RequestClean'].tolist()[
+            len(df_current_sub_group)-1])
+        else:
+            sub_group_3_id.append('NA')
+            sub_group_3_example_a.append('NA')
+            sub_group_3_example_b.append('NA')
+        
+        # determine if there is a fourth subgroup
+        if max_sub_group_id - min_sub_group_id > 2:
+            # aggregate the subgroup4id
+            sub_group_4_id.append(min_sub_group_id+3)
+            # aggregate the subgroup4examplea
+            df_current_sub_group = df_current_group.query(
+                'SecondaryGroupID == '+str(min_sub_group_id+3))
+            sub_group_4_example_a.append(df_current_sub_group['RequestClean'].tolist()[0])
+            # aggregate the subgroup4exampleb
+            sub_group_4_example_b.append(df_current_sub_group['RequestClean'].tolist()[
+            len(df_current_sub_group)-1])
+        else:
+            sub_group_4_id.append('NA')
+            sub_group_4_example_a.append('NA')
+            sub_group_4_example_b.append('NA')
+        
+        # determine if there is a fifth subgroup
+        if max_sub_group_id - min_sub_group_id > 3:
+            # aggregate the subgroup5id
+            sub_group_5_id.append(min_sub_group_id+4)
+            # aggregate the subgroup5examplea
+            df_current_sub_group = df_current_group.query(
+                'SecondaryGroupID == '+str(min_sub_group_id+4))
+            sub_group_5_example_a.append(df_current_sub_group['RequestClean'].tolist()[0])
+            # aggregate the subgroup5exampleb
+            sub_group_5_example_b.append(df_current_sub_group['RequestClean'].tolist()[
+            len(df_current_sub_group)-1])
+        else:
+            sub_group_5_id.append('NA')
+            sub_group_5_example_a.append('NA')
+            sub_group_5_example_b.append('NA')
 
-        # # aggregate the SubGroup3ID
-        # if max_sub_group_id - min_sub_group_id > 1:
-        #     sub_group_3_id.append(min_sub_group_id+2)
-        # else:
-        #     sub_group_3_id.append('NA')
+    # print(primary_group_id)
+    # print(primary_group_name)
+    # print(sub_group_1_id)
+    # print(sub_group_1_example_a)
+    # print(sub_group_1_example_b)
+    # print(sub_group_2_id)
+    # print(sub_group_2_example_a)
+    # print(sub_group_2_example_b)
+    # print(sub_group_3_id)
+    # print(sub_group_3_example_a)
+    # print(sub_group_3_example_b)
+    # print(sub_group_4_id)
+    # print(sub_group_4_example_a)
+    # print(sub_group_4_example_b)
+    # print(sub_group_5_id)
+    # print(sub_group_5_example_a)
+    # print(sub_group_5_example_b)
 
-        # # aggregate the SubGroup3Name
+    # convert the lists to a dataframe
+    df_groups_final = pd.DataFrame({
+        'PrimaryGroupID': primary_group_id,
+        'PrimaryGroupName': primary_group_name,
+        'SubGroup1ID': sub_group_1_id,
+        'SubGroup1ExampleA': sub_group_1_example_a,
+        'SubGroup1ExampleB': sub_group_1_example_b,
+        'SubGroup2ID': sub_group_2_id,
+        'SubGroup2ExampleA': sub_group_2_example_a,
+        'SubGroup2ExampleB': sub_group_2_example_b,
+        'SubGroup3ID': sub_group_3_id,
+        'SubGroup3ExampleA': sub_group_3_example_a,
+        'SubGroup3ExampleB': sub_group_3_example_b,
+        'SubGroup4ID': sub_group_4_id,
+        'SubGroup4ExampleA': sub_group_4_example_a,
+        'SubGroup4ExampleB': sub_group_4_example_b,
+        'SubGroup5ID': sub_group_5_id,
+        'SubGroup5ExampleA': sub_group_5_example_a,
+        'SubGroup5ExampleB': sub_group_5_example_b})
+    # print(df_groups_final.info())
+    # print(df_groups_final)
 
-        # # aggregate the SubGroup4ID
-        # if max_sub_group_id - min_sub_group_id > 2:
-        #     sub_group_4_id.append(min_sub_group_id+3)
-        # else:
-        #     sub_group_4_id.append('NA')
-
-        # # aggregate the SubGroup4Name
-
-        # # aggregate the SubGroup5ID
-        # if max_sub_group_id - min_sub_group_id > 3:
-        #     sub_group_5_id.append(min_sub_group_id+4)
-        # else:
-        #     sub_group_5_id.append('NA')
-
-        # # aggregate the SubGroup5Name
-
-    print(primary_group_id)
-    print(primary_group_name)
-    print(sub_group_1_id)
-    print(sub_group_1_example_a)
-    print(sub_group_1_example_b)
+    # save results to csv file
+    df_groups_final.to_csv('PHASE4_groups_final.csv', index=False)
 
     return 0
 
